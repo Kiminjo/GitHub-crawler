5428274e70eeeb6516bee5756c2cbca8dc3927ed
Kiminjo <deulseong1121@gmail.com>
2021-03-10 01:07:50 +0900

    move string variables to 'crawling_material' file

--- Github_crawling.py
+++ Github_crawling.py
@@ -5,6 +5,7 @@ Created on Mon Oct 26 17:28:14 2020
 @author: Injo Kim
 """
 from github import Github
+from material import crawling_material
 import pandas as pd
 from tqdm import tqdm
 
@@ -13,15 +14,15 @@ def url_organizer(url) :
     return url[0:8] + url[12:23] + url[29:]
 
 
-def search_by_keyword(keywords) :
+def search_by_keyword(keywords, topic) :
     
     # watchers have error -> print stargazer data 
     matrix = []
     
     for keyword in tqdm(keywords) :
-        for period in periods :
+        for period in crawling_material.periods :
             query = '+'.join([keyword]) +' created:' + period[0] + '..' + period[1]
-            result = g.search_repositories(query, sort='stars', order='desc')
+            result = git.search_repositories(query, sort='stars', order='desc')
                 
             print(f'\n\n Found {result.totalCount} repo(s)')
             print(query)
@@ -36,34 +37,22 @@ def search_by_keyword(keywords) :
                     owner_type = 'organization'
                 
                 row = [repo.id, repo.name, repo.owner.id, repo.owner.name, owner_type, repo.created_at, repo.updated_at, repo.get_topics(), repo.language, 
-                       contributors, len(contributors), repo.stargazers_count, repo.forks_count, url_organizer(url)]
+                       contributors, len(contributors), repo.stargazers_count, repo.forks_count, topic, url_organizer(url)]
                     
                 matrix.append(row)
 
-    return pd.DataFrame(matrix, columns=column)
-
+    return pd.DataFrame(matrix, columns=crawling_material.column)
 
 
 if __name__ == '__main__' :
    
     # set constant 
     ACCESS_TOKEN = open('material/access_token.txt', 'r').read()
-    topics = ['nlp', 'image-processing']
-    keywords = {'nlp' : ['nlp', 'natural language processing'], 'image-processing' : ['vision', 'image processing']}
-    g = Github(ACCESS_TOKEN)
+    git = Github(ACCESS_TOKEN)
 
-    global column, periods
-    
-    column = ['repo_id', 'repo_name', 'owner_id', 'owner_name', 'owner_type', 'create_date', 'update_date', 'topics', 'language', 'contributors', 'contributor_counts',  
-             'stargazer_counts', 'forker_counts', 'url']
-
-    periods = [['2015-01-01', '2015-06-30'], ['2015-07-01', '2015-12-31'], ['2016-01-01', '2016-06-30'], ['2016-07-01', '2016-12-31'], ['2017-01-01', '2017-06-30'], 
-              ['2017-07-01', '2017-12-31'], ['2018-01-01', '2018-06-30'], ['2018-07-01', '2018-12-31'], ['2019-01-01', '2019-06-30'], ['2019-07-01', '2019-12-31'], 
-              ['2020-01-01', '2020-06-30'], ['2020-07-01', '2020-12-31'], ['2021-01-01', '2021-03-10']]
-    
-    for topic in topics :
-        data = search_by_keyword(keywords[topic])
+    for topic in crawling_material.topics :
+        data = search_by_keyword(crawling_material.keywords[topic], topic)
         data.to_csv('crawled_data/' + topic + '.csv', index=False)
 
-
+    del topic, git
         
---
+++ material/crawling_material.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 22:19:14 2021
+
+@author: user
+"""
+
+    
+# variable declare
+ 
+periods = [['2015-01-01', '2015-06-30'], ['2015-07-01', '2015-12-31'], ['2016-01-01', '2016-06-30'], ['2016-07-01', '2016-12-31'], ['2017-01-01', '2017-06-30'], 
+           ['2017-07-01', '2017-12-31'], ['2018-01-01', '2018-06-30'], ['2018-07-01', '2018-12-31'], ['2019-01-01', '2019-06-30'], ['2019-07-01', '2019-12-31'], 
+           ['2020-01-01', '2020-06-30'], ['2020-07-01', '2020-12-31'], ['2021-01-01', '2021-03-10']]
+
+column = ['repo_id', 'repo_name', 'owner_id', 'owner_name', 'owner_type', 'create_date', 'update_date', 'topics', 'language', 'contributors', 'contributor_counts',  
+         'stargazer_counts', 'forker_counts', 'keyword', 'url']
+
+topics = ['nlp', 'image-processing']
+
+keywords = {'nlp' : ['nlp', 'natural language processing'], 'image-processing' : ['vision', 'image processing']}
