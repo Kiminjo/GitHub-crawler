a9308a3e7cf1983c08cfbaaebf787f3f4a0e79b2
Kiminjo <deulseong1121@gmail.com>
2021-03-10 00:45:13 +0900

    remove print term and automates topics and time periods

--- Github_crawling.py
+++ Github_crawling.py
@@ -9,19 +9,22 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def search_by_keyword(keywords, g) :
+def url_organizer(url) :
+    return url[0:8] + url[12:23] + url[29:]
+
+
+def search_by_keyword(keywords) :
     
     # watchers have error -> print stargazer data 
     matrix = []
     
     for keyword in tqdm(keywords) :
         for period in periods :
-            #query = '+'.join(subset) + '+in:readme+in:description+state:comments:>3+created:>2019-04-29'
             query = '+'.join([keyword]) +' created:' + period[0] + '..' + period[1]
             result = g.search_repositories(query, sort='stars', order='desc')
-            
                 
             print(f'\n\n Found {result.totalCount} repo(s)')
+            print(query)
             
             for repo in tqdm(result) :
                 contributors = [contributor.id for contributor in repo.get_contributors()]
@@ -31,27 +34,9 @@ def search_by_keyword(keywords, g) :
                     owner_type = 'user'
                 else :
                     owner_type = 'organization'
-                    
-                '''
-                print(repo.id)
-                print(repo.name)
-                print(repo.owner.id)
-                print(repo.owner.name)
-                print(owner_type)
-                print(repo.created_at)
-                print(repo.updated_at)
-                print(repo.get_topics())
-                print(repo.language)
-                print(contributors)
-                print(len(contributors))
-                print(repo.stargazers_count)
-                print(repo.forks_count)
-                print(url[0:8] + url[12:23] + url[29:])         # Remove .api and repos/
-                print('----------------------------------')
-                '''
                 
                 row = [repo.id, repo.name, repo.owner.id, repo.owner.name, owner_type, repo.created_at, repo.updated_at, repo.get_topics(), repo.language, 
-                       contributors, len(contributors), repo.stargazers_count, repo.forks_count, url[0:8] + url[12:23] + url[29:]]
+                       contributors, len(contributors), repo.stargazers_count, repo.forks_count, url_organizer(url)]
                     
                 matrix.append(row)
 
@@ -63,7 +48,8 @@ if __name__ == '__main__' :
    
     # set constant 
     ACCESS_TOKEN = open('material/access_token.txt', 'r').read()
-    nlp_keywords = ['nlp', 'natural language processing']
+    topics = ['nlp', 'image-processing']
+    keywords = {'nlp' : ['nlp', 'natural language processing'], 'image-processing' : ['vision', 'image processing']}
     g = Github(ACCESS_TOKEN)
 
     global column, periods
@@ -75,7 +61,9 @@ if __name__ == '__main__' :
               ['2017-07-01', '2017-12-31'], ['2018-01-01', '2018-06-30'], ['2018-07-01', '2018-12-31'], ['2019-01-01', '2019-06-30'], ['2019-07-01', '2019-12-31'], 
               ['2020-01-01', '2020-06-30'], ['2020-07-01', '2020-12-31'], ['2021-01-01', '2021-03-10']]
     
-    data = search_by_keyword(nlp_keywords, g)
+    for topic in topics :
+        data = search_by_keyword(keywords[topic])
+        data.to_csv('crawled_data/' + topic + '.csv', index=False)
 
 
         
